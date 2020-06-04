5422f93bade14d470cd102c892ff242c90cec30a
brthzz <brthzz@gmail.com>
2020-06-04 12:44:41 +0200

    crypto_hash.py basics

---
+++ crypto_hash.py
@@ -0,0 +1,17 @@
+import hashlib
+import json
+
+def crypto_hash(data):
+    """
+    Return a sha-256 hash of the given data.
+    """
+    # for stringify data, 'json' has been imported
+    stringified_data = json.dumps(data)
+
+    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+
+def main():
+    print(f"crypto_hash('foo'): {crypto_hash('foo')} ")
+
+if __name__ == "__main__":
+    main()
