02761514237dba3bdd5f01d0ebfde9425b1d542a
brthzz <brthzz@gmail.com>
2020-09-19 16:42:13 +0200

    setting up transaction API (backend/app), serialize public_key (json to be sendable over HTTP)

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -60,6 +60,13 @@ class Transaction:
 
         self.input = self.create_input(sender_wallet, self.output)
 
+    def to_json(self):
+        """
+        Serialize the transaction.
+        """
+        return self.__dict__
+        
+    @staticmethod
     def is_valid_transaction(transaction):
         """
         Validate a transaction.
