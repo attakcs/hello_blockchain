8c8d1018d09850c22eb84951fd66c1a273b0a10f
brthzz <brthzz@gmail.com>
2020-10-22 22:43:23 +0200

    FE: Blockchain, Block Components / Nanosecond Variable

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import logo from '../assets/logo.png';
+import { API_BASE_URL } from '../config';
+import Blockchain from './Blockchain';
 
 function App() {
   const [ walletInfo, setWalletInfo ] = useState({});
   
   useEffect(() => {
-    fetch('http://localhost:5000/wallet/info')
+    fetch(`${API_BASE_URL}/wallet/info`)
       .then(response => response.json())
       .then(json => setWalletInfo(json));
   }, []);
@@ -17,9 +19,12 @@ function App() {
       <img className="logo" src={logo} alt="app-logo" />
       <br />
       <div className="WalletInfo">
+        <h3>Wallet info :</h3>
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
       </div>
+      <br />
+      <Blockchain />
     </div>
   );
 }
